6408c0468905f07651bfd0c2c4a24ac99e03ca7f
foxdev-git <nodemagma8@gmail.com>
2025-11-30 00:11:31 +0100

    Added Functions

--- src/Main.php
+++ src/Main.php
@@ -5,7 +5,30 @@ declare(strict_types=1);
 namespace foxdev\AntiVoid;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerMoveEvent;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener {
 
+    public function onEnable(): void {
+        $this->saveDefaultConfig();
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onPlayerMove(PlayerMoveEvent $event): void {
+        $player = $event->getPlayer();
+        $world = $player->getWorld();
+        $worldName = $world->getFolderName();
+        $y = $player->getPosition()->getY();
+
+        $config = $this->getConfig();
+        $voidY = $config->get($worldName);
+
+        if ($voidY !== null && is_numeric($voidY) && $y < (int)$voidY) {
+            $defaultWorld = $this->getServer()->getWorldManager()->getDefaultWorld();
+            if ($defaultWorld !== null) {
+                $player->teleport($defaultWorld->getSpawnLocation());
+            }
+        }
+    }
 }
